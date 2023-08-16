5c24f483855b85308f95d0cdd2decf1a6af3e3fa
IsisRobbs <isisrobbs21@gmail.com>
2023-08-16 18:07:07 -0500

    Fixing javascript folders and working on api not loading

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ const cityInput = document.getElementById('cityInput');
 const searchButton = document.getElementById('searchButton');
 const weatherContainer = document.getElementById('weatherContainer');
 
-searchButton.addEventListener('click', async () => {
+searchButton.addEventListener("click", () => {
     const cityName = cityInput.value;
     if (cityName.trim() !== "") {
-        try {
-            const response = await fetch(`/weather/${cityName}`);
-            const data = await response.json();
-
-            const temperature = data.main.temp;
-            const description = data.weather[0].description;
-            weatherContainer.innerHTML = `
-                <h2>${cityName}</h2>
-                <p>${temperature}°C, ${description}</p>
-            `;
-        } catch (error) {
-            console.error("Error fetching weather data:", error);
-            weatherContainer.innerHTML = "Error fetching weather data";
-        }
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`)
+            .then(response => response.json())
+            .then(data => {
+                const temperature = data.main.temp;
+                const description = data.weather[0].description;
+                weatherContainer.innerHTML = `
+                    <h2>${cityName}</h2>
+                    <p>${temperature}°C, ${description}</p>
+                `;
+            })
+            .catch(error => {
+                console.error("Error fetching weather data:", error);
+                weatherContainer.innerHTML = "Error fetching weather data";
+            });
     }
 });
